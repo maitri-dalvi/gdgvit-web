2b2f7090add9223e54798aa5dde6442efb4e3512
maitri-dalvi <maitridalvi2004@gmail.com>
2025-06-12 21:31:51 +0530

    add: Interviews Section

---
+++ src/app/interviews/page.tsx
@@ -0,0 +1,14 @@
+import Footer from "@/components/Footer"
+import Navbar from "@/components/Navbar"
+
+const Interview = () => {
+  return (
+    <>
+    <Navbar />
+    <div className="p-80 pl-170">Under Development</div>
+    <Footer />
+    </>
+  )
+}
+
+export default Interview
